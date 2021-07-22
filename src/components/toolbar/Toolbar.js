7db99c3f6334327e8eaf5af5bab82db931740e95
Sergeev Yuri <Sergeev.Y.D@sberbank.ru>
2021-07-22 22:44:25 +0300

    Finish base table logic.

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -3,10 +3,10 @@ import {ExcelComponent} from '../../core/ExcelComponent'
 export class Toolbar extends ExcelComponent {
   static className = 'excel__toolbar'
 
-  constructor($root) {
+  constructor($root, options = {}) {
     super($root, {
       name: 'Toolbar',
-      listeners: ['click']
+      ...options
     })
   }
 
@@ -32,6 +32,4 @@ export class Toolbar extends ExcelComponent {
     </div>
     `
   }
-
-  onClick() {}
 }
