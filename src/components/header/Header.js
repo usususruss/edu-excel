3ac7e9357eea734870d1ad0d05917199a02d87c2
Sergeev Yuri <Sergeev.Y.D@sberbank.ru>
2021-08-07 00:14:27 +0300

    Finish redux and table logic.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,8 @@
 import {ExcelComponent} from '../../core/ExcelComponent'
+import {changeTitle} from '../../redux/actions'
+import {$} from '../../core/dom'
+import {defaultTitle} from '../../constants'
+import {debounce} from '../../core/utils'
 
 export class Header extends ExcelComponent {
   static className = 'excel__header'
@@ -6,13 +10,19 @@ export class Header extends ExcelComponent {
   constructor($root, options = {}) {
     super($root, {
       name: 'Header',
+      listeners: ['input'],
       ...options
     });
   }
 
+  prepare() {
+    this.onInput = debounce(this.onInput, 300)
+  }
+
   toHTML() {
+    const title = this.store.getState().title || defaultTitle
     return `
-      <input type="text" value="Новая таблица" class="input" />
+      <input type="text" value="${title}" class="input" />
 
       <div>
           <div class="button">
@@ -23,5 +33,10 @@ export class Header extends ExcelComponent {
           </div>
       </div>
     `
+  }
+
+  onInput(event) {
+    const $target = $(event.target)
+    this.$dispatch(changeTitle($target.text()))
   }
 }
---
+++ src/core/createStore.js
@@ -0,0 +1,22 @@
+export function createStore(rootReducer, initialState = {}) {
+  let state = rootReducer({...initialState}, {type: '__INIT__'})
+  let listeners = []
+
+  return {
+    subscribe(fn) {
+      listeners.push(fn)
+      return {
+        unsubscribe() {
+          listeners = listeners.filter(f => f !== fn)
+        }
+      }
+    },
+    dispatch(action) {
+      state = rootReducer(state, action)
+      listeners.forEach(listener => listener(state))
+    },
+    getState() {
+      return JSON.parse(JSON.stringify(state))
+    }
+  }
+}
